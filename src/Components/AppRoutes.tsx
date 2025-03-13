fc8e8b6db4742ab7c61ceb4800daebc500c0a820
DingoDev <randilea@outlook.com>
2025-03-13 16:58:01 -0600

    Building the grid

--- src/Components/AppRoutes.tsx
+++ src/Components/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Hello from './Hello';
+import BooksGrid from './BooksGrid'
 
 const Views = () => {
     return (
         <BrowserRouter>
         <Routes>
             <Route path="/hello" element={<Hello />} />
+            <Route path="/books" element={<BooksGrid />} />
         </Routes>
         </BrowserRouter>
     )
---
+++ src/Components/BooksGrid.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+function BooksGrid() {
+    return (
+    <div className='book-grid'>
+    <h1>Here is the book grid!</h1>
+    </div>
+    )
+}
+
+export default BooksGrid;
